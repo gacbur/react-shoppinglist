0ae4ec826b6892761c309b520f4ac0f1137dca00
Kacper <kacper0775@gmail.com>
2020-11-20 17:42:58 +0100

    add initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight, faChevronLeft, faTimes, faPlus } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [listItems, addListItems] = useState([
+    { id: 0, quantity: 1, text: 'ogórek', isDone: false },
+    { id: 1, quantity: 4, text: 'Mars', isDone: false },
+    { id: 2, quantity: 1, text: 'ser', isDone: false },
+  ])
+
+  const [inputValue, addInputValue] = useState('')
+  const [inputEmpty, addInputEmpty] = useState(false)
+
+  const handleInputValueOnChange = (e) => {
+    const value = e.target.value
+    addInputValue(value)
+  }
+
+  const handleAddToList = () => {
+
+    if (inputValue !== '') {
+      const Item = {
+        id: listItems.length,
+        quantity: 1,
+        text: inputValue,
+        isDone: false,
+      }
+      console.log(Item)
+      console.log(listItems)
+      const newItems = [...listItems, Item]
+      addListItems(newItems)
+      addInputValue('')
+      addInputEmpty(false)
+    }
+
+    else {
+      addInputEmpty(true)
+    }
+  }
+
+  const handleDeleteItem = (id) => {
+    let listItemsCopy = [...listItems]
+
+    let newlListItems = listItemsCopy.filter(item => item.id !== id)
+    addListItems(newlListItems)
+  }
+
+  const handleQuantityPlus = (index) => {
+    const newItems = [...listItems]
+
+    newItems[index].quantity++
+    addListItems(newItems)
+  }
+
+  const handleQuantityMinus = (index) => {
+    const newItems = [...listItems]
+
+    if (newItems[index].quantity > 1) {
+      console.log(newItems[index].quantity)
+      newItems[index].quantity--
+      addListItems(newItems)
+    }
+  }
+
+  const handleDone = (id) => {
+    const newItems = [...listItems]
+    newItems[id].isDone = !newItems[id].isDone
+    addListItems(newItems)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="addtolist">
+        <div className="add-el">
+          <input type='text' maxlength="20" value={inputValue} onChange={handleInputValueOnChange} placeholder={inputEmpty ? 'Write first, then add!' : "Write something!"}></input>
+          <button onClick={handleAddToList} className="add-el-btn"><FontAwesomeIcon icon={faPlus} /></button>
+        </div>
+      </div>
+      <div className="applist">
+        {listItems.map(listItem => (
+          <div className="el-cnt">
+            <div className="task-text">
+              <p className={listItem.isDone ? 'done' : 'notdone'} onClick={() => handleDone(listItem.id)} key={listItem.id}>{listItem.text}</p>
+            </div>
+            <div className="inc-dec-btn">
+              <FontAwesomeIcon onClick={() => handleQuantityMinus(listItem.id)} className="icon-btn" icon={faChevronLeft} />
+              <p>{listItem.quantity}</p>
+              <FontAwesomeIcon onClick={() => handleQuantityPlus(listItem.id)} className="icon-btn" icon={faChevronRight} />
+              <button className="del-btn" onClick={() => handleDeleteItem(listItem.id)}><FontAwesomeIcon icon={faTimes} /></button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
